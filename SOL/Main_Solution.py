7e3a52bfa522a8a744c3657fbe07384e6725935c
Rahul Yalamanchili <yalaman9@msu.edu>
2018-04-29 21:31:16 -0400

    Added a test

--- SOL/Main_Solution.py
+++ SOL/Main_Solution.py
@@ -1,11 +1,18 @@
 # Driver program for students to try the default test cases
-from ProjectXX_Solution import HashTable
+from SOL.ProjectXX_Solution import Heap
 
 TEST_CASE = "\nTest Case {}:"
 
 
 def test_case_1():
     print(TEST_CASE.format(1))
+    test1 = Heap()
+    for i in range(30, -4, -3):
+        test1.insert(i)
+    print(test1)
+    for i in range(7):
+        test1.remove_min()
+    print(test1)
 
 
 def test_case_2():
